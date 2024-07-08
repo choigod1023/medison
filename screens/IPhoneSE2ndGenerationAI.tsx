242bf79df596970ec501b1ec7ca32c1bcdaa0ae0
choigod1023 <choigod10234@gmail.com>
2024-07-08 16:42:57 +0900

    ai sf

--- screens/IPhoneSE2ndGenerationAI.tsx
+++ screens/IPhoneSE2ndGenerationAI.tsx
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     lineHeight: 22,
     fontSize: FontSize.bodyRegular_size,
-    fontFamily: "SF Pro",
+    fontFamily: "SFProText",
   },
   topRowPosition: {
     right: 6,
